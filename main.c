5f9f7f78f38042d261a74d439eb68a04472d5200
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-21 14:31:25 -0600

    added chan type, tok type, and more complete lexing.
    The chan type is currently useless but hopes to emulate the channel functionality in go.
    
    The tok type will be used as the medium for transport of tokens across the channel, from the lexer to the parser or some other thread running concurrently.
    
    The lexer is now somewhat more complete, lexing strings of +|-|<|>| characters.

--- main.c
+++ main.c
@@ -3,27 +3,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// memset
-#include <string.h>
-
-// integer types
-#include <stdint.h>
-
-// read/write
-#include <unistd.h>
-
-// memory mapping for code generation and execution
-#include <sys/mman.h>
+#include <string.h> // memset
+#include <stdint.h> // integer types
+#include <unistd.h> // read/write
+#include <pthread.h> // threading
+#include <sys/mman.h> // memory mapping for code generation and execution
 
 typedef struct {
 	int type;
 	char *msg;
 } tok;
 
+// concurrent, mutex-based channel
+// similar to channels in Go
+typedef struct {
+	void *data;
+	pthread_mutex_t lock;
+} chan;
+
+// init channel
+chan *chan_init() {
+	chan *c = malloc(sizeof (chan));
+	return c;
+}
+
 typedef struct {
 	size_t size;
 	int len;
+	int llen; // lexed len
 	char *lexed;
 } lex;
 
@@ -32,6 +39,7 @@ lex *lex_init(size_t sz) {
 	lex *l = malloc(sizeof (lex));
 	l->size = sz;
 	l->len = 0;
+	l->llen = 0;
 	l->lexed = malloc(sz); // number of bytes
 	return l;
 }
@@ -43,23 +51,37 @@ void lex_free(lex *l) {
 }
 
 char *lex_emit(lex *l) {
-	memcpy()
+	char *copy = malloc(l->len + 1);
+	memcpy(copy, l->lexed, l->len);
+	copy[l->len] = '\0'; // null terminate
+	l->len = 0; // reset length
+	l->llen = 0;
+	return copy;
 }
 
 char lex_next(lex *l) {
 	if (l->len >= l->size) {
-		l->len *= 2;
-		realloc(l->lexed, l->len);
+		l->size *= 2;
+		realloc(l->lexed, l->size);
 	}
-	char c = getchar();
-	if (c != EOF) {l->lexed[l->len++] = c;}
-	return c;
+	if (l->llen > l->len) {
+		return l->lexed[l->len++];
+	} else {
+		char c = getchar();
+		// printf("next! got: %c\n", c);
+		if (c != EOF) {
+			l->lexed[l->len++] = c;
+			l->llen++; // increase lexed length
+		}
+		return c;
+	}
 }
 
 void lex_back(lex *l) {
 	if (l->len > 0) {
 		l->len--;
 	}
+	// printf("back! dropped: %c\n", l->lexed[l->len]);
 }
 
 char lex_peek(lex *l) {
@@ -68,20 +90,23 @@ char lex_peek(lex *l) {
 	return c;
 }
 
-void *lex_gt(lex *l) {
-	while ((c = lex_next(l)) != EOF && c == '>') {
+void *lex_all(lex *l);
 
-	}
+void *lex_gt(lex *l) {
+	char c;
+	while ((c = lex_next(l)) != EOF && (c == '>' || c == '<' || c == '+' || c == '-')) {}
 	lex_back(l);
+	printf("lexed: %s\n", lex_emit(l));
+	return lex_all;
 }
 
 void *lex_all(lex *l) {
 	char c;
 	while ((c = lex_peek(l)) != EOF) {
-		switch(c) {
-			case '>':
-				printf("ha!\n");
-				return (void *) lex_gt;
+		if (c == '>' || c == '<' || c == '+' || c == '-') {
+			return lex_gt;
+		} else {
+			return NULL;
 		}
 	}
 	return NULL;
