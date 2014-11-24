0fd812dfed7849ff41ebc9769cae273b914aecdb
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-23 19:10:00 -0600

    proper formatting of error messages

--- bf.c
+++ bf.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h> // for exit
 #include <stdarg.h> // ... args
+#include <string.h>
+
+static char *format(char *fmt) {
+	// append newline to fmt
+	int len = strlen(fmt);
+
+	if (len < 0) {
+		return NULL; // error
+	} else {
+		// add one for newline
+		char *msg = malloc(len + 1);
+
+		if (msg == NULL) {
+			return NULL; // error
+		} else {
+
+			// copy message
+			msg = memcpy(msg, fmt, len);
+
+			if (msg == NULL) {
+				return NULL; // error
+			} else {
+				// append newline, null terminate
+				msg[len - 1] = '\n';
+				msg[len] = '\0';
+
+				return msg;
+			}
+		}
+	}
+}
 
 // takes a string message, prints it to stderr,
 // then exits with a failure.
@@ -10,8 +41,17 @@ void fail(char *fmt, ...) {
 	// handle failure message
 	va_list args;
 	va_start (args, fmt);
-	vfprintf (stderr, fmt, args);
-	va_end (args);
+
+	char *msg;
+	if ((msg = format(fmt)) == NULL) {
+		fprintf(stderr, "fail: formatting of error message failed\n.");
+		va_end(args);
+	} else {
+		// print error.
+		vfprintf (stderr, msg, args);
+		va_end (args);
+		free(msg);
+	}
 
 	// exit unhappily.
 	exit(EXIT_FAILURE);
@@ -23,6 +63,15 @@ void err(char *fmt, ...) {
 	// handle failure message
 	va_list args;
 	va_start (args, fmt);
-	vfprintf (stderr, fmt, args);
-	va_end (args);
+
+	char *msg;
+	if ((msg = format(fmt)) == NULL) {
+		fprintf(stderr, "err: formatting of error message failed\n.");
+		va_end(args);
+	} else {
+		// print error.
+		vfprintf (stderr, msg, args);
+		va_end (args);
+		free(msg);
+	}
 }
