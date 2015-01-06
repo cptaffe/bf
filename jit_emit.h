b8fa98f964f2c9d9eb12a4f177fbd8f0cda2e612
Connor Taffe <cpaynetaffe@gmail.com>
2015-01-06 13:55:07 -0600

    uneeded memcpy's removed

--- jit_emit.h
+++ jit_emit.h
@@ -21,7 +21,7 @@ static inline void emit_add(bf_jit *j, uint8_t num) {
 	char add[] = {ADD_LIT8};
 	memcpy(&j->exec[j->exec_pos], add, sizeof(add));
 	j->exec_pos += sizeof(add);
-	memcpy(&j->exec[j->exec_pos], &num, sizeof(num));
+	j->exec[j->exec_pos] = num;
 	j->exec_pos += sizeof(num);
 }
 
@@ -29,7 +29,7 @@ static inline void emit_add_rsi(bf_jit *j, uint8_t num) {
 	char add[] = {ADD_LIT8_SI};
 	memcpy(&j->exec[j->exec_pos], add, sizeof(add));
 	j->exec_pos += sizeof(add);
-	memcpy(&j->exec[j->exec_pos], &num, sizeof(num));
+	j->exec[j->exec_pos] = num;
 	j->exec_pos += sizeof(num);
 }
 
@@ -49,7 +49,7 @@ static inline void emit_loop(bf_jit *j, uint8_t num) {
 	char loop[] = {LOOP};
 	memcpy(&j->exec[j->exec_pos], loop, sizeof(loop));
 	j->exec_pos += sizeof(loop);
-	memcpy(&j->exec[j->exec_pos], &num, sizeof(num));
+	j->exec[j->exec_pos] = num;
 	j->exec_pos += sizeof(num);
 }
 
@@ -59,15 +59,15 @@ static inline void emit_write(bf_jit *j, uint64_t fd) {
 	char mov[] = {MOV_AX_64};
 	memcpy(&j->exec[j->exec_pos], mov, sizeof(mov));
 	j->exec_pos += sizeof(mov);
-	uint64_t n = SYSCALL_WRITE;
-	memcpy(&j->exec[j->exec_pos], &n, sizeof(n));
-	j->exec_pos += sizeof(n);
+	uint64_t s = SYSCALL_WRITE;
+	memcpy(&j->exec[j->exec_pos], &s, sizeof(s));
+	j->exec_pos += sizeof(s);
 
 	// rdi = 1 (stdout)
 	char mov1[] = {MOV_DI_64};
 	memcpy(&j->exec[j->exec_pos], mov1, sizeof(mov1));
 	j->exec_pos += sizeof(mov1);
-	memcpy(&j->exec[j->exec_pos], &fd, sizeof(fd));
+	j->exec[j->exec_pos] = fd;
 	j->exec_pos += sizeof(fd);
 
 	// rsi is already pointed at the correct memory
@@ -75,9 +75,8 @@ static inline void emit_write(bf_jit *j, uint64_t fd) {
 	char mov2[] = {MOV_DX_64};
 	memcpy(&j->exec[j->exec_pos], mov2, sizeof(mov2));
 	j->exec_pos += sizeof(mov2);
-	n = 1;
-	memcpy(&j->exec[j->exec_pos], &n, sizeof(n));
-	j->exec_pos += sizeof(n);
+	j->exec[j->exec_pos] = (uint8_t) 1;
+	j->exec_pos += sizeof(uint8_t);
 
 	char syscall[] = {SYSCALL};
 	memcpy(&j->exec[j->exec_pos], syscall, sizeof(syscall));
@@ -87,7 +86,7 @@ static inline void emit_write(bf_jit *j, uint64_t fd) {
 	char mov3[] = {MOV_64};
 	memcpy(&j->exec[j->exec_pos], mov3, sizeof(mov3));
 	j->exec_pos += sizeof(mov3);
-	memcpy(&j->exec[j->exec_pos], &j->si, sizeof(j->si));
+	j->exec[j->exec_pos] = j->si;
 	j->exec_pos += sizeof(j->si);
 }
 
